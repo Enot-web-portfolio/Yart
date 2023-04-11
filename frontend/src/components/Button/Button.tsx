2a4d1471fe3fee48b1ae0375db0b57234e9ac2f4
Irkatyman <tymanira@mail.ru>
2023-04-11 18:58:09 +0500

    Компонент Кнопка

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react'
+import {Button as ButtonAntd} from 'antd'
+import {ButtonProps} from 'antd/es/button/button'
+import './Button.scss'
+import {ButtonBackgroundColor} from './types'
+import {typedMemo} from 'src/core/utils/typed-memo'
+
+type Props = ButtonProps & {
+  // Ф-ция при клике
+  onClick: () => void;
+  // Тип кнопки
+  type: "primary" | "default" | "link";
+  // Текст внутри кнопки
+  text?: string;
+  // Размер кнопки
+  size?: "middle" | "small";
+  // Цвет заливки кнопки
+  backgroundColor?: ButtonBackgroundColor;
+}
+
+// Компонент Кнопка
+const ButtonComponent: FC<Props> = ({
+                                      backgroundColor = ButtonBackgroundColor.Primary,
+                                      size = "middle",
+                                      ...props
+                                    }: Props) => {
+  return <ButtonAntd {...props}
+                     className={`button ${backgroundColor}_color ${size}_size`}>
+    {props.text || ""}
+  </ButtonAntd>
+}
+
+// Компонент Кнопка
+export const Button = typedMemo(ButtonComponent);
---
+++ frontend/src/components/Button/types.ts
@@ -0,0 +1,8 @@
+/** Цвет заливки кнопки с типом primary
+ * Primary - голубой, основной;
+ * Secondary - фиолетовый, второстепенный
+ */
+export enum ButtonBackgroundColor {
+  Primary = "primary",
+  Secondary = "secondary"
+}
