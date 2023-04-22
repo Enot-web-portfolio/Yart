1eeee9fcb336e5dff0e18428387e41351601f439
Irkatyman <tymanira@mail.ru>
2023-04-22 19:13:26 +0500

    FT tag component

---
+++ frontend/src/components/Tag/Tag.tsx
@@ -0,0 +1,25 @@
+import React, { FC, PropsWithChildren } from 'react';
+
+import { Tag as TagAntd, TagProps } from 'antd';
+
+import { typedMemo } from '../../core/utils/typed-memo';
+
+import classes from './Tag.module.scss';
+
+type Props = Readonly<TagProps & PropsWithChildren & {
+
+  /** Цвет текст тега. */
+  textColor: string;
+}>;
+
+// Компонент Тег
+const TagComponent: FC<Props> = (props: Props) => (
+  <TagAntd {...props}
+    className={classes.tag}
+    style={{ color: props.textColor }}>
+    {props.children}
+  </TagAntd>
+);
+
+// Компонент Тег
+export const Tag = typedMemo(TagComponent);
