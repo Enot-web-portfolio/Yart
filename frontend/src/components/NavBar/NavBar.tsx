ab05c93e82d5a70082b410d7581fb9c77b087e76
Irkatyman <tymanira@mail.ru>
2023-04-30 16:46:45 +0500

    Правки /FT-Add-menu-component

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -2,12 +2,12 @@ import { FC, memo } from 'react';
 
 import classes from './NavBar.module.css';
 
-// TODO: Добавить сслыки на страницы
+// TODO: Добавить ссылки на страницы
 
-const NavBarComponent: FC = () => <nav className={ `${classes['navbar-panel']}` }>
-  <a className={ `${classes['navbar-panel__link']}` }>О нас</a>
-  <a className={ `${classes['navbar-panel__link']}` }>Работы</a>
-  <a className={ `${classes['navbar-panel__link']}` }>Люди</a>
+const NavBarComponent: FC = () => <nav className={`${classes['navbar-panel']}`}>
+  <a className={`${classes['navbar-panel__link']}`}>О нас</a>
+  <a className={`${classes['navbar-panel__link']}`}>Работы</a>
+  <a className={`${classes['navbar-panel__link']}`}>Люди</a>
 </nav>;
 
 export const NavBar = memo(NavBarComponent);
