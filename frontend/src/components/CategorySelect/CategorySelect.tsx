33421b4fc28e96f669e557666634fa2ad0bd75ca
Irkatyman <tymanira@mail.ru>
2023-04-30 16:58:22 +0500

    Правки

--- frontend/src/components/CategorySelect/CategorySelect.tsx
+++ frontend/src/components/CategorySelect/CategorySelect.tsx
@@ -4,21 +4,21 @@ import { typedMemo } from 'src/core/utils/typed-memo';
 import { Category } from 'src/core/models/category';
 import { Typography } from 'antd';
 
-import './CategorySelect.scss';
+import classes from './CategorySelect.module.scss';
 
 const { Text } = Typography;
 
 type Props = Readonly<{
 
   /** Массив категорий навыков. */
-  categories: Category[];
+  categories: readonly Category[];
 
   /** Ф-ция изменения выбранных категорий. */
   onChange: (selectedIds: number[]) => void;
 }>;
 
 // Компонент Панель выбора категорий навыков (фильтр по категориям)
-const CategorySelectComponent: FC<Props> = (props: Props) => {
+const CategorySelectComponent: FC<Props> = props => {
   // Категории
   const [categories, setCategories] = useState(props.categories);
 
@@ -35,13 +35,13 @@ const CategorySelectComponent: FC<Props> = (props: Props) => {
     props.onChange(newCategories.filter(category => category.checked).map(category => category.id));
   };
 
-  return (<div className={'category_select'}>
+  return (<div className={classes.category_select}>
     {categories.map((category, i) =>
-      <div className={`category_select__item ${category.checked ? 'checked' : ''}`}
+      <div className={`${classes.category_select__item} ${category.checked ? classes.checked : ''}`}
         key={i}
         onClick={() => onToggle(category.id)}
         style={{ backgroundColor: category.checked ? category.backgroundColor : undefined }}>
-        <Text className={'category_select__item_name'}
+        <Text className={classes.category_select__item_name}
           style={{ color: category.checked ? category.fontColor : undefined }}>
           {category.name}
         </Text>
