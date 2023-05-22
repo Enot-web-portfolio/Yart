eb13a4ba80f18b2c53bc80ae75d6584eeff5e07e
Irkatyman <tymanira@mail.ru>
2023-05-22 20:05:22 +0500

    Изменен адрес апи + правка селекта скиллов

--- frontend/src/components/SkillsSelect/SkillsSelect.tsx
+++ frontend/src/components/SkillsSelect/SkillsSelect.tsx
@@ -1,12 +1,14 @@
-import React, {FC} from 'react';
-import {typedMemo} from 'src/core/utils/typed-memo';
-import {Spin, Typography} from 'antd';
-import classes from './SkillsSelect.module.scss';
-import {ErrorResult} from "../ErrorResult";
-import {useSeparatedSkills} from "../../core/services/hooks/useSeparateSkills";
-import {Skill} from "../../core/models/skill";
+import React, { FC } from 'react';
+import { typedMemo } from 'src/core/utils/typed-memo';
+import { Spin, Typography } from 'antd';
 
-const {Text} = Typography;
+import { ErrorResult } from '../ErrorResult';
+import { useSeparatedSkills } from '../../core/services/hooks/useSeparateSkills';
+import { Skill } from '../../core/models/skill';
+
+import classes from './SkillsSelect.module.scss';
+
+const { Text } = Typography;
 
 type Props = Readonly<{
 
@@ -14,37 +16,48 @@ type Props = Readonly<{
   onChange: (selectedIds: number[]) => void;
 }>;
 
-/** Компонент Панель выбора категорий навыков (фильтр по категориям). */
+/**
+ * Компонент Панель выбора категорий навыков (фильтр по категориям).
+ * @param props
+ */
 const SkillsSelectComponent: FC<Props> = props => {
   /** Категории для выбора в панели. */
-  const {skills, isLoading, error, setSkills} = useSeparatedSkills<Skill>((skill) => skill);
+  const { skills, isLoading, error, setSkills } = useSeparatedSkills<Skill>(skill => skill);
 
-  /** Ф-ция выбора/развыбора категории. */
+  /**
+   * Ф-ция выбора/развыбора категории.
+   * @param skillId
+   */
   const onToggle = (skillId: number) => {
-    setSkills(skills => {
-      const updatedSkills = (skills || []).map(skill => {
-        if (skill.id == skillId) {
-          skill.checked = !skill.checked;
+    setSkills(curSkills => {
+      const updatedSkills = (curSkills ?? []).map(skill => {
+        const newSkill = { ...skill };
+        if (newSkill.id === skillId) {
+          newSkill.checked = !newSkill.checked;
         }
-        return skill;
-      })
+        return newSkill;
+      });
 
       props.onChange(updatedSkills.filter(skill => skill.checked).map(skill => skill.id));
       return updatedSkills;
     });
   };
 
-  if (isLoading) return <Spin/>;
-  if (skills === null) return <ErrorResult/>;
+  if (isLoading) {
+    return <Spin/>;
+  }
+  if (skills === null) {
+    return <ErrorResult/>;
+  }
   return (
     <div className={classes.category_select}>
       {skills.map((skill, i) =>
         <div className={`${classes.category_select__item} ${skill.checked ? classes.checked : ''}`}
-             key={i}
-             onClick={() => onToggle(skill.id)}
-             style={{backgroundColor: skill.checked ? skill.backgroundColor : undefined}}>
+          key={i}
+          onClick={() => onToggle(skill.id)}
+          style={{ backgroundColor: skill.checked ? skill.backgroundColor : undefined }}>
           <Text className={classes.category_select__item_name}
-                style={{color: skill.checked ? skill.fontColor : undefined}}>
+            style={{ color: skill.checked ? skill.fontColor : undefined }}>
             {skill.name}
           </Text>
         </div>)}
